ef3d0f5dd71a70209b0c4ca495c9aabd9b5c837a
Zoom <caidenleggett@gmail.com>
2025-04-27 12:06:23 -0400

    update examples/inputTable.lua

--- examples/inputTable.lua
+++ examples/inputTable.lua
@@ -1,5 +1,19 @@
 local input = {
   meta = {
-    
+    bundleID = "com.example.example",
+    tools = {
+      rotate = true
+      flip = true
+    },
+  },
+  objects = {
+    [1] = {
+      meta = {
+        name = "block"
+        texture = "/Shared/path/to/texture/"
+        canRotate = true
+        canFlip = true
+      }
+    }
   }
 }
