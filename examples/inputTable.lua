68eaf2710b62dbc1cea98b44b78b7c2fe6824230
Zoom <caidenleggett@gmail.com>
2025-04-27 12:23:23 -0400

    update examples/inputTable.lua

--- examples/inputTable.lua
+++ examples/inputTable.lua
@@ -1,18 +1,29 @@
 local input = {
   meta = {
     bundleID = "com.example.example",
+    name = "My Game",
+    -- banner is an image with the same dimensions as on the catalog
+    banner = "/Shared/path/to/banner"
     tools = {
       rotate = true
       flip = true
-    },
+      settings = true
+    }
   },
   objects = {
     [1] = {
       meta = {
-        name = "block"
+        name = "block",
         texture = "/Shared/path/to/texture/"
-        canRotate = true
-        canFlip = true
+      },
+      settings = {
+        canRotate = true,
+        canFlip = true,
+        rotations = [0,90,180,270],
+        -- You can define custom settings for an object for your game to use.
+        custom = {
+          slippery = "bool"
+        }
       }
     }
   }
